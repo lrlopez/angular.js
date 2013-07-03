6ce1070d2a83d7b0c0ad8b0acebc6770385a23ad
Luis Ramón López <lrlopez@gmail.com>
2013-07-04 01:37:34 +0200

    fix(angular-mocks): Fix missing parameter in flush

--- src/ngMock/angular-mocks.js
+++ src/ngMock/angular-mocks.js
@@ -1487,9 +1487,11 @@ angular.mock.$TimeoutDecorator = function($delegate, $browser) {
    * @description
    *
    * Flushes the queue of pending tasks.
+   *
+   * @param {number=} number of milliseconds to flush. See {@link #defer.now}
    */
-  $delegate.flush = function() {
-    $browser.defer.flush();
+  $delegate.flush = function(delay) {
+    $browser.defer.flush(delay);
   };
 
   /**
